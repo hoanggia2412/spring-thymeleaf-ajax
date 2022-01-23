84a2dfe82657a938477daf0b754c54ce6fa892b9
hoanggia <hoanggia1224@gmail.com>
2022-01-24 00:59:00 +0700

    update register - add item into list at server

--- src/main/java/com/ezbytes/controller/DemoController.java
+++ src/main/java/com/ezbytes/controller/DemoController.java
@@ -1,32 +1,25 @@
 package com.ezbytes.controller;
 
 import java.io.Serializable;
-import java.net.URI;
-import java.net.http.HttpResponse;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.Builder;
 import lombok.Builder.Default;
-import lombok.Data;
 import lombok.Getter;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class DemoController {
-	List<Obj> models =null;
+	private List<Obj> models = new ArrayList<>();
 	@PostConstruct
 	private void init() {
 		Obj obj1 = Obj.builder().name("TR").code("012").number(5).range("5m").block("B01").build();
@@ -34,7 +27,12 @@ public class DemoController {
 		Obj obj3 = Obj.builder().name("DF").code("014").number(3).range("11m").block("C01").build();
 		Obj obj4 = Obj.builder().name("WE").code("015").number(4).range("7m").build();
 		Obj obj5 = Obj.builder().name("GG").code("016").number(9).block("E01").build();
-		models = Arrays.asList(obj1,obj2,obj3,obj4,obj5);
+		//models = Arrays.asList(obj1,obj2,obj3,obj4,obj5);
+		models.add(obj1);
+		models.add(obj2);
+		models.add(obj3);
+		models.add(obj4);
+		models.add(obj5);
 	}
 	
 	@GetMapping
@@ -42,7 +40,7 @@ public class DemoController {
 		model.addAttribute("models", models);
 		return "home";
 	}
-	@GetMapping("{code}")
+	@GetMapping("/{code}")
 	public String get(@PathVariable String code, Model model) {
 		Obj ojb = models.stream().filter(item -> item.getCode().equals(code)).findFirst()
 				.orElse(Obj.builder().name("TC").code("013").number(2).build());
@@ -50,17 +48,17 @@ public class DemoController {
 		return "components/details :: details";		
 	}
 	
-	@GetMapping("register")
+	@GetMapping("/register")
 	public String renderRegister(Model model) {
 		
 		return "components/model_";
 	}
 	
-	@PostMapping("register")
-	public ResponseEntity<?> renderRegister(@RequestBody Obj request,Model model) {
+	@PostMapping("/register")
+	public String renderRegister(Obj request,Model model) {
+		System.out.println(request);
 		models.add(request);
-		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/register").toUriString());
-		return ResponseEntity.created(uri).body(request);
+		return "redirect:/";
 	}
 	
 	@Getter
@@ -72,15 +70,15 @@ public class DemoController {
 		private static final long serialVersionUID = 1L;
 		private String name;
 		private String code;
-		private int number;
+		private Integer number;
 		@Default
 		private boolean status = true;
 		@Default
-		private int height = 0;
+		private Integer height = 0;
 		@Default
-		private int width = 5;
+		private Integer width = 5;
 		@Default
-		private int weight = 4;
+		private Integer weight = 4;
 		@Default
 		private String range = "4m";
 		@Default
